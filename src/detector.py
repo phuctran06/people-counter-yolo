3a76c0e439a461087e4681ea2e82c77d5ad240c9
phuctran06 <phuc.tran.it06@gmail.com>
2026-09-07 21:46:40 +0700

    Update detector

---
+++ src/detector.py
@@ -0,0 +1,20 @@
+from ultralytics import YOLO
+
+
+model = YOLO("yolo11n.pt")
+
+def detect_people(frame):
+    results = model(frame)
+
+    people = []
+
+    for result in results:
+        for box in result.boxes:
+            class_id = int(box.cls[0])  
+
+            if class_id == 0:
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+
+                people.append((x1, y1, x2, y2))
+
+    return people
