18035e926b932c790703c8b92a777cc76a006c10
phuctran06 <phuc.tran.it06@gmail.com>
2026-09-08 12:46:55 +0700

    Add detector

--- src/detector.py
+++ src/detector.py
@@ -14,7 +14,8 @@ def detect_people(frame):
 
             if class_id == 0:
                 x1, y1, x2, y2 = map(int, box.xyxy[0])
+                confidence = float(box.conf[0])
 
-                people.append((x1, y1, x2, y2))
+                people.append((x1, y1, x2, y2, confidence))
 
     return people
