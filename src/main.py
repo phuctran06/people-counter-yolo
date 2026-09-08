407b6e2bd1e69d483bad363efd10ba9c5908f48b
phuctran06 <phuc.tran.it06@gmail.com>
2026-09-08 14:33:56 +0700

    Update main

--- src/main.py
+++ src/main.py
@@ -62,28 +62,12 @@ def is_inside_zone(person, zone):
 
     x1, y1, x2, y2, _ = person
     center_x, center_y = (x1 + x2) // 2, (y1 + y2) // 2
-    
 
     return cv2.pointPolygonTest(zone, (center_x, center_y), False) >= 0
 
 
-def select_zone(event, x, y, flags, param):
-    global zone_points, zone_selected
-
-    if event == cv2.EVENT_LBUTTONDOWN:
-        zone_points.append((x, y))
-
-    elif event == cv2.EVENT_RBUTTONDOWN:
-        if len(zone_points) >= 3:
-            zone_selected = True
-        else:
-            print("Please select at least 3 points to form a zone.")
-
 
 if __name__ == "__main__":
-
-    zone_points = []
-    zone_selected = False
 
     video = cv2.VideoCapture("videos/input.mp4")
 
@@ -91,3 +75,4 @@ if __name__ == "__main__":
     get_video_info(video)
 
     video.release()
+
