c549572baada85fd1c3fc639f6a6b199ef12cdd7
phuctran06 <phuc.tran.it06@gmail.com>
2026-09-08 21:23:07 +0700

    Update track people def

--- src/detector.py
+++ src/detector.py
@@ -3,20 +3,27 @@ from ultralytics import YOLO
 
 model = YOLO("yolo11n.pt")
 
-def detect_people(frame):
-    results = model(frame)
+
+
+def track_people(frame):
+
+    results = model.track(frame, persist=True)
 
     people = []
 
     for result in results:
         for box in result.boxes:
-            class_id = int(box.cls[0])  
+
+            class_id = int(box.cls[0])
 
             if class_id == 0:
+
                 x1, y1, x2, y2 = map(int, box.xyxy[0])
                 confidence = float(box.conf[0])
 
-                people.append((x1, y1, x2, y2, confidence))
+                if box.id is not None:
+                    track_id = int(box.id[0])
+
+                    people.append((x1, y1, x2, y2, confidence, track_id))
 
     return people
-
