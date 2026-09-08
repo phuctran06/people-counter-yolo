1deaddb19b1cb04d3b01651023a24d96fc45f8d3
phuctran06 <phuc.tran.it06@gmail.com>
2026-09-08 10:47:46 +0700

    Add def is_inside_zone

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import cv2
 
+from detector import detect_people
+
 
 def read_video(video_path):
     video = cv2.VideoCapture(video_path)
@@ -9,6 +11,10 @@ def read_video(video_path):
 
         if not ret:
             break
+        
+        people = detect_people(frame)
+
+        frame = draw_people(frame, people)
 
         cv2.imshow("People Counter", frame)
 
@@ -20,6 +26,7 @@ def read_video(video_path):
 
 
 def get_video_info(video):
+    
     fps = video.get(cv2.CAP_PROP_FPS)
     width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -32,16 +39,39 @@ def get_video_info(video):
     print(f"Frame count: {frame_count}")
     print(f"Duration: {duration:.2f} seconds")
 
-def draw_people(frame, people):
+
+def draw_people(frame, people, zone):
+
+    cv2.polylines(frame, [zone], isClosed=True, color=(0, 0, 255), thickness=2)
+
     for person in people:
-        x1, y1, x2, y2 = person
+        x1, y1, x2, y2, confidence = person
+
+        # Calculate the center of the bounding box
+        center_x, center_y = (x1 + x2) // 2, (y1 + y2) // 2
 
         cv2.rectangle(frame,(x1, y1),(x2, y2),(0, 255, 0),2)
 
+        cv2.putText(frame,f"Person: {confidence:.2f}",(x1, y1 - 10),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,255,0),20 )
+        cv2.circle(frame, (center_x, center_y), 5, (0, 0, 255), -1)
     return frame
 
-video = cv2.VideoCapture("videos/input.mp4")
 
-get_video_info(video)
+def is_inside_zone(person, zone):
+    x1, y1, x2, y2, _ = person
+    center_x, center_y = (x1 + x2) // 2, (y1 + y2) // 2
+    
 
-video.release()
+    return cv2.pointPolygonTest(zone, (center_x, center_y), False) >= 0
+
+
+
+
+if __name__ == "__main__":
+
+    video = cv2.VideoCapture("videos/input.mp4")
+
+    read_video("videos/input.mp4")
+    get_video_info(video)
+
+    video.release()
